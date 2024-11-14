32a17378ae9f7bfabd6fa88b6af65d42c8a22041
Br1-O <bruno.ortuno2@gmail.com>
2024-11-14 16:49:22 -0300

    Added: Clase4

---
+++ Clase4/desafio8.py
@@ -0,0 +1,31 @@
+"""
+Un profesor debe cargar un conjunto de notas de exámenes.
+
+Debemos realizar un programa que nos permita CARGAR un conjunto de notas de exámenes utilizando una LISTA.
+
+Luego MOSTRAR el contenido de cada uno de los valores que existen en la LISTA.
+
+PD.: Usted define si el programa le solicita al usuario la cantidad de notas a cargar o que ingrese el valor 0 para finalizar la carga, por ejemplo.
+"""
+
+#RESOLUCIÓN:
+
+print("Ingrese las notas a cargar: \n (Cuando haya finalizado la carga de notas ingrese 'FIN')\n")
+
+grade = 0
+grade_list = []
+exit = False
+
+while not exit:
+    grade = input("Ingrese una nota: ")
+    if grade == "FIN" or grade == "fin":
+        exit = True
+        continue
+    grade_list.append(grade)
+
+if len(grade_list) > 0 :
+    print("\nLas notas ingresadas fueron:")
+    for grade in grade_list:
+        print(grade)
+else:
+    print("\nNo se han ingresado notas.")
---
+++ Clase4/desafio9.py
@@ -0,0 +1,30 @@
+"""
+Retomar el ejercicio del desafío anterior pero solo CARGAR en la LISTA aquellas notas que estén entre 6 y 10 (inclusive)
+ 
+MOSTRAR el contenido de cada uno de los valores que existen en la LISTA.
+"""
+
+print("Ingrese las notas a cargar: \n (Cuando haya finalizado la carga de notas ingrese 'FIN' para mostrar aquellas entre 6 y 10)\n")
+
+#RESOLUCIÓN
+
+grade = 0
+minor_grade = 6
+greater_grade = 10
+grade_list = []
+exit = False
+
+while not exit:
+    grade = input("Ingrese una nota: ")
+    if grade == "FIN" or grade == "fin":
+        exit = True
+        continue
+    if minor_grade <= int(grade) <= greater_grade:
+        grade_list.append(grade)
+
+if len(grade_list) > 0 :
+    print("\nLas notas ingresadas, que son mayores a 6 y menores a 10, fueron:")
+    for grade in grade_list:
+        print(grade)
+else:
+    print("\nNo se han ingresado notas.")
